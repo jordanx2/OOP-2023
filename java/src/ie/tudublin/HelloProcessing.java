a59e09264f8adb91e23c6731f42e9b295cf9f249
jordanmurray <jordanmooney9@gmail.com>
2023-01-30 12:19:47 +0000

    saved state

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,13 +11,25 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		// colorMode(HSB);
 		background(0);
 
 	}
 	
 	public void draw()
 	{	
-		
+		stroke(255);
+		line(10,10,100,100); // x1, y1, x2, y2
+		circle(300, 250, 70); // cx, cy, d
+		fill(0, 250, 0);
+		rect(110, 300, 20, 100); // tlx, tly,  w, h
+		stroke(127);
+		fill(0, 0, 250);
+		noStroke();
+		noFill();
+		strokeWeight(1);
+		triangle(40, 90, 300, 20, 80, 70);
+		triangle(X, Y, X, Y, X, Y);
+		fill(0, 0, 250);
 	}
 }
