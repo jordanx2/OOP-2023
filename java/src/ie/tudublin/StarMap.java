b22ca13ce464425e4f6291ff389d82658fb34c5c
jordanmurray <jordanmooney9@gmail.com>
2023-02-13 16:06:33 +0000

    comments added

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -12,14 +12,50 @@ public class StarMap extends PApplet
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
+		stroke(255);
+		strokeWeight(2);	
 		smooth();
+		
+	}
+
+	public void drawGrid(){
+		float border = 50f;
+
+		// determines the number of lines to be drawn
+		int count = 10;
+
+		// Defines the gaps between all of the lines
+		float gap = (width - (border * 2.0f)) / count;
+
+		for(int i = -5; i <= 5; i++){	
+			float x =  border + (gap * (i + 5));
+
+			/*
+			 * Here is an explaintation of the parameters: 
+			 * 	the parameters are basically x1, y1, x2, y2
+			 * 	x1, y1 
+			 * 	-> draws from the current x position to the top of sketch, current position + up
+			 	x2, y2
+				-> draws from the current x position to the bottom of the sketch, current position + down 
+			 */
+			// verticle line
+			line(border, x, width - border, x);
+
+			// horizontal
+			line(x, border, x, height - border);
+			
+		}
+	}
+
+
+
+	public void drawGrid2(){
 		
 	}
 
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+		drawGrid();	
 	}
 }
