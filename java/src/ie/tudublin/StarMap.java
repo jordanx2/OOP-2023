f06fa4be5690cd1b0c231282780f9f72042cf5e8
jordanmurray <jordanmooney9@gmail.com>
2023-02-20 15:10:06 +0000

    stuff

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -8,6 +8,8 @@ import processing.data.TableRow;
 
 public class StarMap extends PApplet
 {
+	ArrayList<Star> stars = new ArrayList<Star>();
+
 	public void settings()
 	{
 		size(500, 500);
@@ -18,9 +20,25 @@ public class StarMap extends PApplet
 		background(0);
 		
 		smooth();
-		
 
+		loadStars();
+		printStars();
+	}
 
+	void printStars()
+	{
+		for(int i = 0 ; i < stars.size() ; i ++)
+		{
+			println(stars.get(i));
+		}
+	}
+
+	void displayStars()
+	{
+		for(int i = 0 ; i < stars.size() ; i ++)
+		{
+			stars.get(i).render(this);
+		}
 	}
 
 	public void loadStars()
@@ -35,17 +53,22 @@ public class StarMap extends PApplet
 
 	public void drawGrid()
 	{
-		stroke(255);
-		float border = 50.0f;
+		stroke(0, 255, 255);
+		float border = width * 0.1f;
 
-		int count = 10;
-		float gap = (width - (border * 2.0f)) / (float) count;
 		for(int i = -5 ; i <= 5 ; i ++)
 		{
-			float x = border + (gap * (i + 5));
+			float x = map(i, -5, 5, border, width - border);
 			line(x, border, x, height - border);
 			line(border, x, width - border, x);
+
+			textAlign(CENTER, CENTER);
+			text(i, x, border * 0.5f);
+			text(i, border * 0.5f, x);
 		}
+
+		//float f = map(5, 0, 10, 100, 200);
+		//float f1 = map1(5, 0, 10, 100, 200);
 		
 	}
 
