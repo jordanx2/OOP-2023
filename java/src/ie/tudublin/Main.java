8db6479ca75609e9665775b8a675860dc851bf88
jordanmurray <jordanmooney9@gmail.com>
2023-02-20 11:14:16 +0000

    update

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -39,7 +39,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		loops();
+		starMap();
 	}
 	
 }
--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,9 +1,15 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet
 {
+	ArrayList<Star> stars = new ArrayList<>();
+
 	public void settings()
 	{
 		size(500, 500);
@@ -14,31 +20,57 @@ public class StarMap extends PApplet
 		background(0);
 		
 		smooth();
-		
+		loadStars();
+		printStars();
+	}
 
+	void printStars(){
+		for(Star s : stars){
+			println(s);
+		}
+	}
 
+	public void loadStars(){
+		Table table = loadTable("HabHYG15ly.csv", "header");
+		for(TableRow r:table.rows())
+		{
+			Star s = new Star(r);
+			stars.add(s);
+		}
 	}
 
 
 	public void drawGrid()
 	{
 		stroke(255);
-		float border = 50.0f;
-
-		int count = 10;
-		float gap = (width - (border * 2.0f)) / (float) count;
+		float border = width * 0.1f;
 		for(int i = -5 ; i <= 5 ; i ++)
 		{
-			float x = border + (gap * (i + 5));
+			float x = map(i, -5, 5, border, width - border) ;
 			line(x, border, x, height - border);
 			line(border, x, width - border, x);
+
+			textAlign(CENTER, CENTER);
+			// text(i, x, );
 		}
+
+		// float f = map(5, 0, 10, 100, 200);
+		// float f1 = map1(5, 0, 10, 100, 200);
 		
+	}
+
+	float map1(float a, float b, float c, float d, float e){
+		float r1 = c - b;
+		float r2 = e - d;
+		float howFar = a - b;
+
+		return d + ((howFar / r1) * r2);
+
 	}
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(1);		
 
 		drawGrid();
 	}
