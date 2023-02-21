bc75ad3ebc25595fa6ad035732ce8b5a89ce0070
jordanmurray <jordanmooney9@gmail.com>
2023-02-21 19:19:47 +0000

    track added

--- java/src/ie/tudublin/Audio.java
+++ java/src/ie/tudublin/Audio.java
@@ -1,7 +1,6 @@
 package ie.tudublin;
 
 import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
@@ -12,7 +11,6 @@ public class Audio extends PApplet
     Minim minim;
     AudioPlayer ap;
     AudioBuffer ab;
-    AudioInput ai;
 
 	public void settings()
 	{
@@ -29,14 +27,11 @@ public class Audio extends PApplet
 
         minim = new Minim(this);
 
-        ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
+        // ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
 
-        // ap = minim.loadFile("heroplanet.mp3");
+        ap = minim.loadFile("FadeAway.mp3");
         
-        ab = ai.mix;
         smooth();
-
-		
 	}
 
     public void keyPressed(){
@@ -54,23 +49,23 @@ public class Audio extends PApplet
     }
 
     public void draw(){
-        background(0);
-        
-		float angle = 0, xCord = 0, yCord = 0;
-		float xCenter = width / 2;
-		float yCenter = height / 2;
-		float radius = 200;
+        // background(0);
 
-		// 2πr == 360 degrees
-		for(int i = 0; i < ab.size(); i++){
-            stroke(random(255), 255, 255);
-			angle = radians(i);
-			xCord = xCenter + sin(angle) * radius;
-			yCord = yCenter + cos(angle) * radius;
-            float value = ab.get(i) * 50;
-			ellipse(xCord + value, yCord + value, 2, 2);
-			// line(xCenter, yCenter, xCord, yCord);
-		}
+		// float angle = 0, xCord = 0, yCord = 0;
+		// float xCenter = width / 2;
+		// float yCenter = height / 2;
+		// float radius = 200;
+
+		// // 2πr == 360 degrees
+		// for(int i = 0; i < ab.size(); i++){
+        //     stroke(random(255), 255, 255);
+		// 	angle = radians(i);
+		// 	xCord = xCenter + sin(angle) * radius;
+		// 	yCord = yCenter + cos(angle) * radius;
+        //     float value = ab.get(i) * 50;
+		// 	ellipse(xCord + value, yCord + value, 2, 2);
+		// 	// line(xCenter, yCenter, xCord, yCord);
+		// }
     }
 }
 
