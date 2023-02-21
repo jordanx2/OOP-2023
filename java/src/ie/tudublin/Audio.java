9815a773d97fa8430bad18b05722f18592dc2964
jordanmurray <jordanmooney9@gmail.com>
2023-02-21 21:50:21 +0000

    circle stuff

--- java/src/ie/tudublin/Audio.java
+++ java/src/ie/tudublin/Audio.java
@@ -12,26 +12,25 @@ public class Audio extends PApplet
     AudioPlayer ap;
     AudioBuffer ab;
 
+
 	public void settings()
 	{
 		size(1024, 500);
 	}
 
     int frameSize = 1024;
-
+    float kickSize, snareSize, hatSize;
 	public void setup() {
         noFill();
         stroke(255);
 		colorMode(RGB);
 		background(0);
-
         minim = new Minim(this);
-
         // ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
-
-        ap = minim.loadFile("FadeAway.mp3");
-        
+        ap = minim.loadFile("fadeaway.mp3");
         smooth();
+
+
 	}
 
     public void keyPressed(){
@@ -49,23 +48,28 @@ public class Audio extends PApplet
     }
 
     public void draw(){
-        // background(0);
+        background(0);
 
-		// float angle = 0, xCord = 0, yCord = 0;
-		// float xCenter = width / 2;
-		// float yCenter = height / 2;
-		// float radius = 200;
+		float angle = 0, xCord = 0, yCord = 0;
+		float xCenter = width / 2;
+		float yCenter = height / 2;
+		float radius = 200;
 
-		// // 2πr == 360 degrees
-		// for(int i = 0; i < ab.size(); i++){
-        //     stroke(random(255), 255, 255);
-		// 	angle = radians(i);
-		// 	xCord = xCenter + sin(angle) * radius;
-		// 	yCord = yCenter + cos(angle) * radius;
-        //     float value = ab.get(i) * 50;
-		// 	ellipse(xCord + value, yCord + value, 2, 2);
-		// 	// line(xCenter, yCenter, xCord, yCord);
-		// }
+		// 2πr == 360 degrees
+		for(int i = 0; i < ap.bufferSize(); i++){
+            stroke(random(255), random(255), 255);
+			angle = radians(i);
+			xCord = xCenter + sin(angle) * radius;
+			yCord = yCenter + cos(angle) * radius;
+            float value = ap.mix.get(i) * 30;
+            // line(xCenter, yCenter, xCord, yCord);
+            ellipse(xCord + value, yCord +  value, 2, 2);
+
+		}
+  
+
+
+
     }
 }
 
