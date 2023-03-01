82b2892f414a0c1353f6bf83b62859bfc8c3bbc1
jordanmurray <jordanmooney9@gmail.com>
2023-03-01 16:04:39 +0000

    class work

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -4,6 +4,7 @@ import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import processing.core.PApplet;
 
 public class Audio2 extends PApplet {
@@ -12,8 +13,10 @@ public class Audio2 extends PApplet {
     AudioPlayer ap;
     AudioBuffer ab;
 
+    FFT fft;
+
     public void settings(){
-        size(1024, 1024);
+        size(1024, 800);
     }
 
     public void setup(){
@@ -21,6 +24,7 @@ public class Audio2 extends PApplet {
         ai = m.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ai.mix;
         lerpedBuffer = new float[width];
+        fft = new FFT(width, 44100);
     }
 
     float[] lerpedBuffer; 
@@ -36,11 +40,34 @@ public class Audio2 extends PApplet {
             line(i, half + f, i, half- f);
         }
 
+        fft.forward(ab);
+        stroke(255);
+
+        int heightIndex = 0;
+
+        for(int i = 0; i < fft.specSize() / 2; i++){
+            line(i * 2.0f, height, i * 2.0f, height - fft.getBand(i) * 5.0f);
+            if(fft.getBand(i) > fft.getBand(heightIndex)){
+                heightIndex = i;
+            }
+        }
+
+        fill(255);
+        float freq = fft.indexToFreq(heightIndex);
+        textSize(20);
+        text("Freq: " + freq, 10, 50);
+
+        float y = map(freq, 1000, 2500, height, 0);
+        lerpedY = lerp(lerpedY, y, 0.1f);
+        circle(200, y, 50);
+        circle(200, lerpedY, 200);
+
         // println(map(5, 0, 10, 1000, 2000));
         // println(map1(5, 0, 10, 1000, 2000));
         
     }
 
+    float lerpedY;
     float map1(float a, float b, float c, float d, float e){
         float range1 = c - b;
         float range2 = e - d;
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -30,6 +30,12 @@ public class Main
 	public static void audio1()
 	{
 		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
+
+	public static void audio2()
+	{
+		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Audio2());
     }
 	
@@ -45,7 +51,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		audio1();
+		audio2();
 	}
 	
 }
